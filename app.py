42afd62ccaf8a2466c3cf9045e585d96d84398b2
John Marcus III <j3+demo@jwmarcus.com>
2018-07-20 14:12:19 -0400

    adding remote database configuration

--- app.py
+++ app.py
@@ -2,9 +2,23 @@
 # @jwmarcus-demo
 # app.py - Entry point for application
 
+from confire import Configuration, environ_setting
 from flask import Flask
+import os
+
+# Get database credentials
+class DbConfig(Configuration):
+    CONF_PATHS = ['./db_config.yaml']
+
+# Load settings right away
+settings = DbConfig.load()
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+@app.route('/db_name/')
+def print_db_params():
+    return settings['database'].get("name")
